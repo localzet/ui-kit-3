9f1ca8aaf232de62f807ea66636c148d9fb7ea56
Ivan Zorin <creator@localzet.com>
2026-06-05 21:06:31 +0300

    feat: restructure preview-main.js to import UI components and register them globally

--- demo/preview-main.js
+++ demo/preview-main.js
@@ -1,13 +1,39 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 
-import ZorinUi3 from '../src'
+import '../src/tailwind.css'
+import '../src/sass/app.scss'
+
+import AlertBox from '../src/components/UI/Others/AlertBox.vue'
+import ButtonBase from '../src/components/UI/Buttons/ButtonBase.vue'
+import ColorLabel from '../src/components/UI/Labels/ColorLabel.vue'
+import ListInfo from '../src/components/UI/List/ListInfo.vue'
+import ListInfoItem from '../src/components/UI/List/ListInfoItem.vue'
+import ProgressBar from '../src/components/UI/Others/ProgressBar.vue'
+import SectionTitle from '../src/components/UI/Labels/SectionTitle.vue'
+import SwitchInput from '../src/components/Inputs/SwitchInput.vue'
+import TextLabel from '../src/components/UI/Labels/TextLabel.vue'
 import { liveExamples } from './registry'
 import PreviewApp from './PreviewApp.vue'
 
 Vue.config.productionTip = false
 Vue.use(VueI18n)
-Vue.use(ZorinUi3)
+
+const previewComponents = [
+    AlertBox,
+    ButtonBase,
+    ColorLabel,
+    ListInfo,
+    ListInfoItem,
+    ProgressBar,
+    SectionTitle,
+    SwitchInput,
+    TextLabel,
+]
+
+previewComponents.forEach((component) => {
+    Vue.component(component.name, component)
+})
 
 const i18n = new VueI18n({
     locale: 'en',
